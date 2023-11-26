37de5e2d66c42b3dd1b1e85191e8554ed3b9b102
balevaas <72543214+balevaas@users.noreply.github.com>
2023-11-26 22:05:59 +0700

    Добавьте файлы проекта.

---
+++ Classes/DataBase.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Classes
+{
+    /*
+     ,ProductCategory
+      ,ProductPhoto
+      ,ProductManufacturer
+      ,ProductCost
+      ProductDiscountAmount
+      ,ProductQuantityInStock
+    */
+    public class DataBase
+    {
+        public static string connectionString = @"Data Source=Anastasia-ПК; Initial Catalog=Trade; Integrated Security=True;";
+        private readonly SqlConnection connection = new SqlConnection(connectionString);
+
+        public DataTable SqlSelect(string cmd)
+        {
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();            
+            SqlCommand command = new SqlCommand(cmd, conn);
+            command.ExecuteNonQuery();
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            return dt;
+        }
+
+        /// <summary>
+        /// Метод для открытия соединения с БД
+        /// </summary>
+        public void OpenConnection()
+        {
+            // Если состояние строки закрыто, открываем
+            if (connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        /// <summary>
+        /// Метод для закрытия соединения с БД
+        /// </summary>
+        public void CloseConnection()
+        {
+            // Если состояние строки открыто, закрываем
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        /// <summary>
+        /// Метод для возвращения строки подключения
+        /// </summary>
+        /// <returns></returns>
+        public SqlConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp1.Classes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly DataBase data = new DataBase();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ProductsViewDataGrid();
+
+            ProductsListView();
+        }
+
+        /// <summary>
+        /// Метод для "простого" вывода данных в табличку
+        /// </summary>
+        private void ProductsViewDataGrid()
+        {
+            DataTable dt = data.SqlSelect("select * from Product"); // обращение к методу из класса
+            DGProduct.ItemsSource = dt.DefaultView; // передаем их в дата грид
+            data.CloseConnection(); // закрываем соединение
+        }
+
+        private void ProductsListView()
+        {
+            DataTable dt = data.SqlSelect("Select * from Product");
+            LWProduct.ItemsSource = dt.DefaultView;
+            data.CloseConnection();
+        }
+    }
+}
